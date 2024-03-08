c788d09fbd842ca0d68af50056bba8d1b625351d
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-08 13:51:09 -0800

    update lib

--- engine/lib.js
+++ engine/lib.js
@@ -4,9 +4,9 @@
  */
 export function generateUEID(name) {
     // The randomly generated string will be 8 characters of hex 
-    const LOWER_BOUND = 16**8;
-    const UPPER_BOUND = (16**9)-1;
+    const LOWER_BOUND = 16**7;
+    const UPPER_BOUND = (16**8)-1;
 
-    const generated = Math.random() * (UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND;
-    return `${name}-${generated.toString(16)}`;
+    const generated = Math.floor(Math.random() * (UPPER_BOUND - LOWER_BOUND) + LOWER_BOUND);
+    return `${name}-${Math.floor(generated).toString(16)}`;
 }
