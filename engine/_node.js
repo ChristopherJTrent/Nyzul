c414c609d6ab282af3a594d728645a5ba11eb7ca
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-26 08:17:00 -0700

    fix linter errors

--- engine/_node.js
+++ engine/_node.js
@@ -2,36 +2,36 @@
  * A wrapper around HtmlElement that adds component functionality
  */
 export class NyzulNode {
-    /**
+	/**
      * 
      * @param {HTMLElement} element 
      */
-    constructor(element) {
-        this.element = element;
-    }
-    /**
+	constructor(element) {
+		this.element = element
+	}
+	/**
      * appends an HTMLElement to this node
      * @param {HTMLElement} element 
      */
-    appendChild(element) {
-        this.element.appendChild(element);
-    }
-    /**
+	appendChild(element) {
+		this.element.appendChild(element)
+	}
+	/**
      * appends the render output of a component to the DOM tree of this node
      * @param {Component} component 
      */
-    appendComponent(component) {
-        this.appendChild(component.render());
-    }
-    /**
+	appendComponent(component) {
+		this.appendChild(component.render())
+	}
+	/**
      * appends a node to this object
      * @param {NyzulNode} node
      */
-    appendNode(node) {
-        this.appendChild(node.element);
-    }
+	appendNode(node) {
+		this.appendChild(node.element)
+	}
 
-    finalize() {
-        return this.element
-    }
+	finalize() {
+		return this.element
+	}
 }
