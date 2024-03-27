da7f49aecde3a205769cd0ba249421934d47c02e
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-27 10:47:51 -0700

    Add a component factory

---
+++ engine/componentFactory.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @param {{
+ * 	type?: keyof HTMLElementTagNameMap,
+ * 	id?: string,
+ *  classes?: string[],
+ * 	innerText?: string,
+ * 	attributes?: {[key]: string},
+ *  eventCallbacks?: {
+ * 		type: string,
+ * 		listener: EventListenerOrEventListenerObject,
+ * 		options?: boolean | AddEventListenerOptions
+ * }[]
+ * }} componentDefinition 
+ */
+export default function createElement(componentDefinition) {
+	const element = document.createElement(componentDefinition.type ?? 'div')
+	if (componentDefinition.id) element.id = componentDefinition.id
+	if (componentDefinition.classes) 
+		element.classList.add(componentDefinition.classes)
+	if (componentDefinition.innerText) 
+		element.innerText = componentDefinition.innerText
+	if (componentDefinition.attributes) {
+		for (const [k, v] of Object.entries(componentDefinition.attributes)) {
+			element.setAttribute(k, v)
+		}
+	}
+	if (componentDefinition.eventCallbacks) {
+		for(const def of componentDefinition.eventCallbacks) {
+			element.addEventListener(def.type, def.listener, def.options)
+		}
+	}
+	return element
+}
--- engine/nyzul.js
+++ engine/nyzul.js
@@ -1,4 +1,5 @@
 import { NyzulNode } from './_node.js'
+import createElement from './componentFactory.js'
 /**
  * The root object for all Nyzul projects
  * Every element you add to your page will hang off of this object.
@@ -11,5 +12,23 @@ export default class Nyzul extends NyzulNode {
 	constructor(rootId = 'pageContainer') {
 		super()
 		this.element = document.getElementById(rootId)
+	}
+	/**
+ 	* @param {{
+	* 	type?: keyof HTMLElementTagNameMap,
+	* 	id?: string,
+	*  classes?: string[],
+	* 	innerText?: string,
+	* 	attributes?: {[key]: string},
+	*  eventCallbacks?: {
+	* 		type: string,
+	* 		listener: EventListenerOrEventListenerObject,
+	* 		options?: boolean | AddEventListenerOptions
+	* }[]
+	* }} componentDefinition 
+	* @returns {ReturnType<createElement>}
+	*/
+	static createElement(componentDefinition) {
+		return createElement(componentDefinition)
 	}
 }
